b7f613bb10d5cec058e5b896682d9f41f0023b66
Blackasvel <matthieu.briand@orange.fr>
2014-05-26 17:09:38 +0200

    gyvdb

--- DutOMania/src/test/Test.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("Ceci est un test. Lol !");
-		System.out.println("Ceci est un autre test! LOOOOL");
-	}
-}
