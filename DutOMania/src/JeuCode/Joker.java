1dd76acbd6e1831d6f9da336d4ab1280d55c5645
Blackasvel <matthieu.briand@orange.fr>
2014-05-26 17:59:35 +0200

    Début

--- DutOMania/src/JeuCode/Joker.java
+++ DutOMania/src/JeuCode/Joker.java
@@ -1,5 +1,31 @@
 package JeuCode;
 
 public class Joker {
+	
+	private String nom;
+	private boolean dispo;
+	
+	public Joker(String nom, boolean dispo) {
+		super();
+		this.nom = nom;
+		this.dispo = dispo;
+	}
 
+	
+	public void jokerMoitMoit (String rep1, String rep2, String rep3, String rep4, Question q){
+		
+	}
+	
+	public void joker42(){
+		
+	}
+	
+	public boolean isDispo() {
+		return dispo;
+	}
+
+	public void setDispo(boolean dispo) {
+		this.dispo = dispo;
+	}
+	
 }
