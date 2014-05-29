eb50fcd0b1628da10f548a112747ac8bd6ebf5e4
Alexcouter <alexcouter@gmail.com>
2014-05-29 18:03:46 +0200

    Update FenetreBase.java

--- DutOMania/src/jeuGraphic/FenetreBase.java
+++ DutOMania/src/jeuGraphic/FenetreBase.java
@@ -1,14 +1,16 @@
 package jeuGraphic;
 
 import java.awt.FlowLayout;
+import java.awt.Point;
 import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class FenetreBase extends JFrame {
-	 public FenetreBase(String title, int x, int y) {
+	 public FenetreBase(String title) {
 //	    setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/LogoAplacer.ici")));
 	    setVisible(true);
-	    setSize(x, y);
+	    setSize(800, 600);
 	    setLayout(new FlowLayout(1));
 	    setLocationRelativeTo(null);
 	    setDefaultCloseOperation(EXIT_ON_CLOSE);
