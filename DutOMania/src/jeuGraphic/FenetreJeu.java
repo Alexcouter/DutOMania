5f7443729481a9982214e8183e01a14bf1168966
Alexcouter <alexcouter@gmail.com>
2014-06-01 13:14:59 +0200

    Update FenetreJeu.java

--- DutOMania/src/jeuGraphic/FenetreJeu.java
+++ DutOMania/src/jeuGraphic/FenetreJeu.java
@@ -3,10 +3,16 @@ package jeuGraphic;
 public class FenetreJeu {
 
 	public FenetreJeu(int bouton) {
+		
+		//Lancement du mode normal
+		
 		if (bouton == 1){
 			new FenetreJeuNormal();
 			}
 		else if(bouton == 2){
+			
+			//Lancement du mode survie
+			
 			new FenetreJeuSurvieChoix();
 		}
 		}
