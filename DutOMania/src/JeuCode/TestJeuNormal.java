176fbec2b334ffa018cb0f270ad0e4c7c7c54cc4
jbmorice <jeanbaptiste.morice@gmail.com>
2014-06-04 23:23:46 +0200

    Modif question

---
+++ DutOMania/src/JeuCode/TestJeuNormal.java
@@ -0,0 +1,11 @@
+package JeuCode;
+
+public class TestJeuNormal {
+
+	public static void main(String[] args) {
+		JeuNormal jeu = new JeuNormal();
+		
+		jeu.recupererQuestionAleatoire("");
+	}
+
+}
