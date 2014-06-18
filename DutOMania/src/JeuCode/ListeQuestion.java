7fe0935edf8c53957f4a5ba03bea5b8b196430d4
Blackasvel <matthieu.briand@orange.fr>
2014-06-18 12:03:31 +0200

    classe ListeQuestion

---
+++ DutOMania/src/JeuCode/ListeQuestion.java
@@ -0,0 +1,18 @@
+package JeuCode;
+
+import java.util.ArrayList;
+
+public class ListeQuestion {
+	private ArrayList<Question> listeQuestion;
+
+	public ListeQuestion(ArrayList<Question> listeQuestion) {
+		super();
+		this.listeQuestion = listeQuestion;
+	}
+	
+	
+	public void chargerQuestionListes(){
+		
+	}
+
+}
