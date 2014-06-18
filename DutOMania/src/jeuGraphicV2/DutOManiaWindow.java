327d4ef90c708a4d291aeb42fceaade903a830cb
MisterMikis <jeanbaptiste.morice@gmail.com>
2014-06-18 12:03:24 +0200

    GUI V2

---
+++ DutOMania/src/jeuGraphicV2/DutOManiaWindow.java
@@ -0,0 +1,40 @@
+package jeuGraphicV2;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class DutOManiaWindow extends JFrame {
+	public static MainMenuPanel menuPrincipal = new MainMenuPanel();
+	public static SurvivalChoicePanel menuChoixSurvie = new SurvivalChoicePanel();
+	public static NormalGamePanel ecranJeuNormal = new NormalGamePanel();
+	public static Container cont;
+	
+	public DutOManiaWindow(){
+		super("DutOMania");
+	 	//Permet la création d'une super classe fenêtre
+//	    setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/LogoAplacer.ici")));
+	    
+	    //taille 800x600
+	    
+	    setSize(800, 600);
+//	    setLayout(new FlowLayout(1));
+	    
+	    //Ajout des panels
+	    cont = getContentPane();
+	    cont.add(menuPrincipal);
+	    
+	    //Centré au lancement
+	    
+	    
+	    setLocationRelativeTo(null);
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    
+	    setResizable(false);
+	    setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new DutOManiaWindow();
+	}
+}
